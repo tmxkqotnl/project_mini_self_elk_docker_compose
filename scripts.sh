8557e326fb7453d6cbd85ae9a8cc66847d578e1c
tmxkqotnl <tmxkqotnl@gmail.com>
2022-03-25 17:25:46 +0900

    feat, scripts

--- scripts.sh
+++ scripts.sh
@@ -7,8 +7,18 @@
 # spark
 # kafka
 
-# if [ $# -eq 0 ]; then
-#     echo 1
-# else
-#     docker run --privileged -d $par /sbin/init
-# fi
+if [ $# -eq 0 ]; then
+    echo 1
+elif [ $1 == 'elasticsearch' ]; then
+    docker run --privileged -p 9200 -p 9300 -d --name $1 $1_local /sbin/init
+elif [ $1 == 'kibana' ]; then
+    docker run --privileged -p 5601 -d --name $1 $1_local /sbin/init
+elif [ $1 == 'logstash' ]; then
+    docker run --privileged -p 5044 -d --name $1 $1_local /sbin/init
+elif [ $1 == 'elasticsearch' ]; then
+    docker run --privileged -p 9200 -p 9300 -d --name $1 $1_local /sbin/init
+elif [ $1 == 'elasticsearch' ]; then
+    docker run --privileged -p 9200 -p 9300 -d --name $1 $1_local /sbin/init
+elif [ $1 == 'elasticsearch' ]; then
+    docker run --privileged -p 9200 -p 9300 -d --name $1 $1_local /sbin/init
+fi
